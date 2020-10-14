ca4cd0454f97205a33fc4015aeb4f770a6d68764
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-14 11:48:13 -0700

    Cleaned up axios request, stripped training data for use in neural net.

--- server/main/routes.py
+++ server/main/routes.py
@@ -6,27 +6,27 @@ from bson.json_util import dumps
 main = Blueprint("main", __name__)
 
 
-@main.route("/")
+@main.route("/", methods=["GET", "POST"])
 def home():
     """Show the user the homepage."""
     return render_template("index.html")
+
+
+@main.route("/gettrainingdata")
+def show_training_data():
+    """Return all training data as json to client."""
+    training_data = db.trainingdata.find({})
+    return dumps(training_data)
+
+
+@main.route("/getuserinput", methods=["POST"])
+def get_user_input():
+    pass
 
 
 @main.route("/trainingdata", methods=["POST"])
 def add_training_data():
     """Simple interface to add training data to database from client."""
     training_data = request.json
-    db.trainingdata.insert({"trainingdata": training_data})
-    return dumps({"trainingdata": training_data})
-
-
-@main.route("/gettrainingdata", methods=["GET"])
-def show_training_data():
-    """Return all training data as json to client."""
-    training_data = db.trainingdata.get({})
-    return dumps({"trainingdata": training_data}), 200
-
-
-@main.route("/getuserinput", methods=["POST"])
-def get_user_input():
-    pass
+    db.trainingdata.insert(training_data)
+    return dumps(training_data)
--- server/static/net.js
+++ server/static/net.js
@@ -1,23 +1,35 @@
+const trainingDataArr = []
+
 const trainingData = () => {
   axios({
     "method": "GET",
-    "data": "trainingdata",
+    "data": "data",
     "url": "http://localhost:5000/gettrainingdata"
   })
   .then(response => {
     console.log(response)
+    console.log(response.data)
+    trainingDataArr.push(response.data[0].trainingData.map(data => data.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/['"]+/g, '')))
+    console.log(trainingDataArr)
   })
   .catch(error => {
     console.log(error)
   })
 }
 
-const net = new brain.recurrent.LSTM();
+trainingData()
 
-net.train(trainingData, {
-  iterations: 1500,
-  errorThresh: 0.011,
-  log: (stats) => console.log(stats)
-});
+
+const net = new brain.recurrent.LSTM()
+
+console.log(net)
+
+// console.log(net.train(trainingDataArr))
+
+// net.train(trainingDataArr
+//   // iterations: 1500,
+//   // errorThresh: 0.011,
+//   // log: (stats) => console.log(stats)
+// )
 
 const askAQuestion = prompt('Ask Burroughs a Question')
