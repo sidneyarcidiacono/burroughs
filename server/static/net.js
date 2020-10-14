b1eabbc421df383192b9c7a64ca1482311604ce2
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-14 13:45:29 -0700

    Refactored to async-await for bug fixes

--- server/static/net.js
+++ server/static/net.js
@@ -1,35 +1,52 @@
 const trainingDataArr = []
 
-const trainingData = () => {
-  axios({
-    "method": "GET",
-    "data": "data",
-    "url": "http://localhost:5000/gettrainingdata"
-  })
-  .then(response => {
-    console.log(response)
-    console.log(response.data)
-    trainingDataArr.push(response.data[0].trainingData.map(data => data.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").replace(/['"]+/g, '')))
-    console.log(trainingDataArr)
-  })
-  .catch(error => {
+const config = {
+  hiddenLayers: [3], // array of ints for the sizes of the hidden layers in the network
+  // activation: 'sigmoid', // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh'],
+  // leakyReluAlpha: 0.01, // supported for activation type 'leaky-relu'
+};
+
+const getTrainingData = async () => {
+  try {
+    let res = await axios({
+      "method": "GET",
+      "data": "data",
+      "url": "http://localhost:5000/gettrainingdata"
+    })
+    if (res) {
+      console.log(res)
+      trainingDataArr.push(res.data[0].trainingData.map(data => data.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")))
+      console.log(trainingDataArr)
+    }
+  }
+  catch (error) {
     console.log(error)
-  })
+  }
 }
 
-trainingData()
+// console.log(trainingDataArr)
+
+const trainNet = async () => {
+  await getTrainingData()
+  const net = new brain.recurrent.LSTM(config)
+
+  console.log(`Console logging net ${Object.keys(net)}`)
+
+  net.train(trainingDataArr, {
+    iterations: 1500,
+    errorThresh: 0.011,
+    log: (stats) => console.log(stats)
+  })
+
+  console.log(net.run({'Hello'}))
+}
+
+trainNet()
 
 
-const net = new brain.recurrent.LSTM()
-
-console.log(net)
-
-// console.log(net.train(trainingDataArr))
 
 // net.train(trainingDataArr
 //   // iterations: 1500,
 //   // errorThresh: 0.011,
 //   // log: (stats) => console.log(stats)
 // )
-
-const askAQuestion = prompt('Ask Burroughs a Question')
