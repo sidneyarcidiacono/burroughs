bd6b44ab2d786c4fbe94ad6f5240c1836f77411f
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-15 13:16:21 -0700

    restructuring training data, optimizing neural net

--- server/static/net.js
+++ server/static/net.js
@@ -1,43 +1,122 @@
-const trainingDataArr = []
+const trainingData = [
+"desperation is the raw material of drastic change",
+"when you stop growing you start dying",
+"language is a virus from outer space",
+"nothing is true everything is permitted",
+"a functioning police state needs no police",
+"this is a war universe war all the time",
+"that is its nature",
+"be just and if you cant be just be arbitrary",
+"the face of evil is always the face of total need",
+"exterminate all rational thought",
+"perhaps all pleasure in only relief",
+"like all pure creatures cats are practical",
+"sometimes paranoias just having all the facts",
+"which came first the intestine or the tapeworm",
+"confusion hath fuck his masterpiece",
+"never give anything away for nothing",
+"always take back everything if you possibly can",
+"most people dont see whats going on around them",
+"the young are an alien species",
+"last night i encountered a dream cat",
+"with a very long neck and body like a",
+"human fetus",
+"i don't know how to provide for it",
+"for gods sake keep your eyes open",
+"i project myself out through glasses",
+"and across the street",
+"americans have a special horror",
+"of giving up control",
+"the only possible ethic is to do",
+"what one wants to do",
+"your knowledge of what's going on",
+"can only be superficial",
+"love what is it most natural painkiller",
+"every man has inside him a parasitic being",
+"that is not acting at all to his advantage",
+"our national drug is alcohol",
+"regard any other with a special kind of horror",
+"a functioning police state needs no police",
+"i woke up with someone squeezing my hand",
+"it was my other hand",
+"pregnant women lose their teeth feeding the",
+"stranger junkies lose their yellow fangs",
+"feeding the monkey",
+"junk is the ideal product",
+"the ultimate merchandise",
+"in deep sadness there is no place",
+"for sentimentality",
+"silence is only frightening to people who are",
+"compulsively verbalizing",
+"they boys eat happily",
+"looking into each others eyes",
+"blood runs down their chins",
+"all writing is in fact cutups",
+"a collage of words read heard overheard",
+"what else",
+"the step into space is always a step into the unknown",
+"o death where is thy sting",
+"the man is never on time",
+"always take back everything if you possibly can",
+"always catch the buyer hungry",
+"and always make him wait",
+"the self is like a pimping blackmailing",
+"chauffeur who gets you from here to there",
+"on word lines",
+"how far would people get in physics if",
+"discovery were described as disgusting",
+"dream long enough and dream hard enough",
+"and you will come to know dreaming can",
+"make it so"
+]
+
 
 const config = {
-  hiddenLayers: [3], // array of ints for the sizes of the hidden layers in the network
+  hiddenLayers: [16, 12], // array of ints for the sizes of the hidden layers in the network
 };
+//
+// const getTrainingData = async () => {
+//   try {
+//     let res = await axios({
+//       "method": "GET",
+//       "data": "data",
+//       "url": "http://localhost:5000/gettrainingdata"
+//     })
+//     if (res) {
+//       console.log(res)
+//       console.log(res.data)
+//       const trainingData = res.data
+//       console.log(trainingData)
+//       return trainingData
+//     }
+//   }
+//   catch (error) {
+//     console.log(error)
+//   }
+// }
+//
+// const trainNet = async () => {
+//   await getTrainingData()
+//   const net = new brain.recurrent.LSTM(config)
+//
+//   net.train(trainingData, {
+//     iterations: 1500,
+//     errorThresh: 0.011,
+//     log: (stats) => console.log(stats)
+//   })
+// }
 
-const getTrainingData = async () => {
-  try {
-    let res = await axios({
-      "method": "GET",
-      "data": "data",
-      "url": "http://localhost:5000/gettrainingdata"
-    })
-    if (res) {
-      console.log(res)
-      trainingDataArr.push(res.data[0].trainingData.map(data => data.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")))
-      console.log(trainingDataArr)
-      output = trainingDataArr.filter(output => output.length < 70)
-      console.log(output)
-    }
-  }
-  catch (error) {
-    console.log(error)
-  }
-}
+const net = new brain.recurrent.LSTM(config)
 
 
-const trainNet = async () => {
-  await getTrainingData()
-  const net = new brain.recurrent.LSTM(config)
-
-  net.train(trainingDataArr, {
-    iterations: 3000,
-    errorThresh: 0.011,
-    log: (stats) => console.log(stats)
-  })
-  // console.log(trainingDataArr)
-  output = net.run('Life is')
-
-  console.log(output)
-}
-
-trainNet()
+// net.train(trainingData, {
+//   iterations: 2000,
+//   errorThresh: 0.011,
+//   log: (stats) => console.log(stats)
+// })
+//
+// console.log(net.run("i am"))
+//
+//
+//
+// trainNet()
