dac2ebc62ac7cadcfbf39618418b90c0c0a31748
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-10-16 12:58:19 -0700

    enabled hamburger menu functionality/navigation

--- server/static/index.js
+++ server/static/index.js
@@ -1,1 +1,9 @@
+const hamMenu = document.getElementById('hamburger-menu')
+const sideNav = document.getElementById('side-nav')
 
+const hamMenuHandler = () => {
+  hamMenu.classList.add('invisible')
+  sideNav.classList.remove('invisible')
+}
+
+hamMenu.addEventListener('click', hamMenuHandler)
